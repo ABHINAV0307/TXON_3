7c0583b0374e4a9b6b0af3258bc5a6540a9722b9
Abhinav Tripathi <abhinavtripathi080@gmail.com>
2023-01-21 00:40:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,3 +56,11 @@ function filterTodo(e) {
     const todos = todoList.childNodes;
     todos.forEach(function(todo) {
         switch(e.target.value) {
+            case "all": 
+                todo.style.display = "flex";
+                break;
+            case "completed": 
+                if(todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
